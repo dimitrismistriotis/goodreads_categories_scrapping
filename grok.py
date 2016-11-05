8225ef4f49fb05b26a66505ac94c3c7d12bed14e
dimitrismistriotis <dimitrismistriotis@gmail.com>
2016-11-05 09:27:20 +0000

    To function

--- grok.py
+++ grok.py
@@ -4,14 +4,18 @@
 #
 from bs4 import BeautifulSoup
 
-current_html = ''
-with open('list', 'r') as myfile:
-  current_html = myfile.read()
+def list_file_to_csv_entries(filename):
+  current_html = ''
+  with open('list', 'r') as myfile:
+    current_html = myfile.read()
 
-print(current_html)
+  # print(current_html)
 
-soup = BeautifulSoup(current_html, 'html.parser')
+  soup = BeautifulSoup(current_html, 'html.parser')
 
-for i in soup.select('div.shelfStat'):
-  print(i.select('a.actionLinkLite')[0].string)
-  print(i.select('div.smallText.greyText')[0].string.strip().replace('books', ''))
+  for i in soup.select('div.shelfStat'):
+    category = i.select('a.actionLinkLite')[0].string
+    number_of_books = i.select('div.smallText.greyText')[0].string.replace('books', '').replace(',', '').strip()
+    print('"{0}","{1}"'.format(category, number_of_books))
+
+list_file_to_csv_entries('list')
