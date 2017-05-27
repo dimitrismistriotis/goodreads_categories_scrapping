82a2152f727a404f7f7fe78537940d00419e67fe
Dimitrios Mistriotis <dimitrios@mistriotis.com>
2017-05-27 20:35:02 +0100

    Changes for existing data

--- plot_barchart.py
+++ plot_barchart.py
@@ -2,33 +2,31 @@ import math as math
 import matplotlib.pyplot as plt
 import pandas as pd
 
-data = pd.Series({'cat1': 8200000, 'cat2': 6800123, 'cat3': 7800000, 'cat4': 6000000,
-                 'cat5': 4300000, 'cat6': 4100000, 'cat7': 3800000, 'cat8': 3500000,
-                 'cat9': 3400000, 'cat10': 3300000})
+dataf = pd.read_csv('cagetories_books.csv')
+dataf.columns = ['Category', 'Books']
 
-# data.plot(kind='barh', color='k')
-# Very nice up to here, but not what I want...
-
-dataf = pd.DataFrame(data, columns=['books_published'])
-
-max_value = dataf.max()['books_published']
+# max_value = dataf.max()['books_published']
+max_value = dataf.max()['Books'] # first column... works
 annotation_divisior = 10 ** (math.floor(math.log(max_value, 10)))
 
 dataf['color'] = '#afafaf'
-dataf.loc[dataf['books_published'] == max_value, ['color']] = '#f70e0e'
+dataf.loc[dataf['Books'] == max_value, ['color']] = '#f70e0e'
 #dataf.loc[dataf['books_published'] != max_value, ['color']] = '#afafaf'
 
-dataf = dataf.sort_values(['books_published'], ascending=[True])
+dataf = dataf.sort_values(['Books'], ascending=[True])
 
+top_ten = dataf[-10:]
 #
 # Stack Overflow to the rescue for annotating(!):
 # http://stackoverflow.com/questions/23591254/python-pandas-matplotlib-annotating-labels-above-bar-chart-columns
+#
 
-ax = dataf.plot(kind='barh', legend=False,
-                  color=[dataf['color']],
+ax = top_ten.plot(kind='barh', legend=False,
+                  color=[top_ten['color']],
                   linestyle=None,
+                  x='Category',
                   # title="Books published per category",
-                  figsize=(15, 10), fontsize=20)
+                  figsize=(25, 10), fontsize=20)
 
 for p in ax.patches:
     ax.annotate("%.1f" % (p.get_width() / annotation_divisior),
